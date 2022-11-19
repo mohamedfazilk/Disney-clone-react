2316cad80fafababa4359064353d55056ffe5081
mohamedfazilk <2114921@cognizant.com>
2022-11-19 11:39:01 +0530

    place the logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
    <Header/>
+   <Home/>
     </div>
   );
 }
